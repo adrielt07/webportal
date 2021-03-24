d6a140e7f0ffd713028309631256f61d026b037b
Adriel Tolentino <33245729+adrielt07@users.noreply.github.com>
2021-03-23 23:30:52 -0700

    fix entrypoint - it was failing because it couldn't find web_portal (#14)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-python manage.py migrate web_portal
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module ctrl_web_portal.wsgi
