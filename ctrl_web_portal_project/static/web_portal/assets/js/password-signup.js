13171fecf8ddc1a087295b7d3b8cfe2d99e98b49
Adriel Tolentino <33245729+adrielt07@users.noreply.github.com>
2021-04-20 21:52:24 -0700

    Update signup form (#23)
    * enable the create button if the password matches during account creation
    
    * add eventlistener to both password and confirmed_password field to make sure password matches

--- ctrl_web_portal_project/static/web_portal/assets/js/password-signup.js
+++ ctrl_web_portal_project/static/web_portal/assets/js/password-signup.js
@@ -1,12 +1,15 @@
 (function () {
-    var password = document.getElementById('id_password1');
-    var passwordConfirm = document.getElementById('id_password2');
+    let password = document.getElementById('id_password1');
+    let passwordConfirm = document.getElementById('id_password2');
+    let password_field = document.querySelectorAll('.password_field')
 
-    passwordConfirm.addEventListener('input', function() {
-        if (password.value === passwordConfirm.value) {
-            document.getElementById("signupBtn").disabled = false;
-        } else {
-            document.getElementById("signupBtn").disabled = true;
-        }
+    password_field.forEach(item => {
+        item.addEventListener('input', function() {
+            if (password.value === passwordConfirm.value) {
+                document.getElementById("signupBtn").disabled = false;
+            } else {
+                document.getElementById("signupBtn").disabled = true;
+            }
+        })
     })
 } () );
