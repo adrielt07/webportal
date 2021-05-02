4019968c221dbb2c1a90509bac450501180c8db6
Adriel Tolentino <33245729+adrielt07@users.noreply.github.com>
2021-05-01 22:11:41 -0700

    retrict normal users from seeing the admin cards in the homepage (#32)

--- ctrl_web_portal_project/web_portal/views/homepage_view.py
+++ ctrl_web_portal_project/web_portal/views/homepage_view.py
@@ -8,6 +8,7 @@ class HomepageView(View):
         context = {
             'firstname': request.user.firstname,
             'lastname': request.user.lastname,
-            'title': 'Ctrl-layer Portal Home'
+            'title': 'Ctrl-layer Portal Home',
+            'is_ctrl_admin': request.user.is_ctrl_admin,
         }
         return render(request, 'web_portal/home.html', {'context': context})
