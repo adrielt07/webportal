f4c43b90eda6435e4a1d69f7bd0f5fcbe400fa14
Adriel Tolentino <adriel_tolentino@outlook.com>
2021-05-13 22:18:50 -0700

    removed old schedule app

--- ctrl_web_portal_project/schedule/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from schedule.models import ScheduleModel
-
-# Register your models here.
-class ScheduleModelAdmin(admin.ModelAdmin):
-    list_display = ("id", "company", "user")
-
-admin.site.register(ScheduleModel, ScheduleModelAdmin)
