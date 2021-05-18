42fb2d0415fe223d759071dfed511b32d6d04fc2
Adriel Tolentino <33245729+adrielt07@users.noreply.github.com>
2021-05-18 13:27:29 -0700

    added a schedule list view that will list out all the schedules created for the company (#48)

--- ctrl_web_portal_project/app_schedule/views.py
+++ ctrl_web_portal_project/app_schedule/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import View
+from app_schedule.models import ScheduleModel
 
 
 class ScheduleView(View):
     def get(self, request):
+        schedules = ScheduleModel.objects.filter(company_id=request.user.company_id)
         context = {
             'firstname': request.user.firstname,
             'lastname': request.user.lastname,
             'title': 'Ctrl-layer Portal Home',
-            'is_ctrl_admin': request.user.is_ctrl_admin,
+            'page_title': 'Schedule List',
+            'schedules': schedules,
         }
-        return render(request, 'schedule/schedule_default.html', {'context': context})
+        return render(request, 'schedule/schedule_list.html', {'context': context})
