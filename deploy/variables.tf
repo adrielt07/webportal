d8c4fd97a1df0ea33c5381d4b376ac771ab61d4e
adrielt07 <adrieltolentino@outlook.com>
2022-03-07 23:50:01 -0800

    finish up creation / configuration of ecs using Fargat

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -19,3 +19,15 @@ variable "db_password" {
 variable "bastion_key_name" {
   default = "web_portal_bastion_secret_key"
 }
+
+variable "docker_webportal_image" {
+  description = "Webportal docker image"
+}
+
+variable "docker_proxy_image" {
+  description = "Proxy's docker image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django key"
+}
